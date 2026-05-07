59bf822cb0517af504cdfa5be7f5661609aca01a
carlozserra <carlozhenrique166@gmail.com>
2026-05-07 15:49:04 -0300

    everything equal main e cs-individual

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,13 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  const data = getLocalStorage("so-cart") || [];
+  data.push(product);
+  setLocalStorage("so-cart", data);
 }
-
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
