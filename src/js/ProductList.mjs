14bd82be8b32ebd0982778d1c2e0ba538518c912
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-05-19 20:56:47 -0400

    First commit

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -17,13 +17,13 @@ export function productCardTemplate(product) {
 
   return `
     <li class="product-card">
-      <a href="product_pages/?product=${product.Id}">
+      <a href="product_pages/index.html?product=${product.Id}">
         <img src="${product.Image}" alt="${product.Name}" />
-        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h3 class="card__brand">${product.Brand?.Name || ""}</h3>
         <h2 class="card__name">${product.Name}</h2>
         
         <div class="product-card__price-info">
-          ${discountHTML ? `<p class="discount-badge">${discountHTML}</p>` : ""}
+          ${discountHTML}
           <p class="product-card__price">$${product.FinalPrice}</p>
           ${
             isDiscounted
